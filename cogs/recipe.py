5d995ffe69fd14402f0a71fb447383ed1990d94f
vcokltfre <vcokltfre@gmail.com>
2021-02-07 22:56:25 +0000

    feat: add mkrecipe

--- cogs/recipe.py
+++ cogs/recipe.py
@@ -1,13 +1,16 @@
 from templatebot import Bot
 from discord.ext import commands
-from json import load
-from pathlib import Path
+from json import loads, dumps
 
-recipe_template = """Recipe for {item}
+from utils.text import strip_backticks
+
+recipe_template = """**__Recipe for {item}__**
 Produces: {prod}
 
 This recipe requires:
 {req}
+
+This item is worth **{value}h**
 """
 
 
@@ -17,18 +20,24 @@ class Recipe(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-        with Path("./static/recipes.json").open() as f:
-            self.recipes = load(f)
+    async def get_recipe(self, name: str):
+        r = await self.bot.db.get_recipe(name)
 
-    def get_recipe(self, name: str):
-        for recipe in self.recipes:
-            if recipe["name"] == name:
-                return recipe
-        return None
+        if not r: return None
+
+        return {
+            "name": r[0],
+            "produces": {
+                "amount": r[1],
+                "human": r[2]
+            },
+            "value": r[3],
+            "ingredients": loads(r[4])
+        }
 
     @commands.command(name="recipe")
     async def recipe(self, ctx: commands.Context, *, item: str):
-        r = self.get_recipe(item.lower())
+        r = await self.get_recipe(item.lower())
 
         if not r:
             return await ctx.send(
@@ -38,8 +47,21 @@ class Recipe(commands.Cog):
         req = "\n".join([f"{i['amount']} {i['name']}" for i in r["ingredients"]])
 
         await ctx.send(
-            recipe_template.format(item=item, prod=r["produces"]["human"], req=req)
+            recipe_template.format(item=item, prod=r["produces"]["human"], req=req, value=r["value"])
         )
+
+    @commands.command(name="mkrecipe")
+    @commands.is_owner()
+    async def mkrecipe(self, ctx: commands.Context, *, json: str):
+        json = loads(strip_backticks(json))
+        name = json.get("name")
+        value = json.get("value")
+        qty = json.get("amount")
+        human = json.get("human")
+        ingredients = json.get("ingredients")
+
+        await self.bot.db.create_recipe(name, qty, human, value, dumps(ingredients))
+        await ctx.send("Recipe created!")
 
 
 def setup(bot: Bot):
