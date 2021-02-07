5038bebe5ac8698c641d29c293d05e815d2789ee
vcokltfre <vcokltfre@gmail.com>
2021-02-07 22:36:48 +0000

    fix: update formatting

--- static/init.sql
+++ static/init.sql
@@ -1,35 +1,35 @@
 CREATE TABLE IF NOT EXISTS Users (
-    "id"              BIGINT NOT NULL,
-    "name"            VARCHAR(37) NOT NULL,
-    "badges"          TEXT DEFAULT NULL,
-    "created"         TIMESTAMP NOT NULL DEFAULT NOW(),
-    "banned"          BOOLEAN NOT NULL DEFAULT FALSE,
-    "staff"           BOOLEAN NOT NULL DEFAULT FALSE,
+    "id"                BIGINT NOT NULL,
+    "name"              VARCHAR(37) NOT NULL,
+    "badges"            TEXT DEFAULT NULL,
+    "created"           TIMESTAMP NOT NULL DEFAULT NOW(),
+    "banned"            BOOLEAN NOT NULL DEFAULT FALSE,
+    "staff"             BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS Bakeries (
-    "owner_id"        BIGINT NOT NULL REFERENCES Users ("id"),
-    "name"            VARCHAR(64) NOT NULL,
-    "inventory"       TEXT NOT NULL,
-    "created"         TIMESTAMP NOT NULL DEFAULT NOW(),
-    "total_xp"        BIGINT NOT NULL DEFAULT 0,
-    "owned_h"         BIGINT NOT NULL DEFAULT 250,
+    "owner_id"          BIGINT NOT NULL REFERENCES Users ("id"),
+    "name"              VARCHAR(64) NOT NULL,
+    "inventory"         TEXT NOT NULL,
+    "created"           TIMESTAMP NOT NULL DEFAULT NOW(),
+    "total_xp"          BIGINT NOT NULL DEFAULT 0,
+    "owned_h"           BIGINT NOT NULL DEFAULT 250,
     PRIMARY KEY ("owner_id")
 );
 
 CREATE TABLE IF NOT EXISTS Recipes (
-    "name"            VARCHAR(64) NOT NULL,
-    "produces_q"      INT NOT NULL,
-    "produces_h"      VARCHAR(128) NOT NULL,
-    "value"           BIGINT NOT NULL,
-    "ingredients"     TEXT NOT NULL,
+    "name"              VARCHAR(64) NOT NULL,
+    "produces_q"        INT NOT NULL,
+    "produces_h"        VARCHAR(128) NOT NULL,
+    "value"             BIGINT NOT NULL,
+    "ingredients"       TEXT NOT NULL,
     PRIMARY KEY ("name")
 );
 
 CREATE TABLE IF NOT EXISTS UserItems (
-    "name"          VARCHAR(64) NOT NULL REFERENCES Recipes ("name"),
-    "userid"          BIGINT NOT NULL REFERENCES Users ("id"),
-    "qty"           BIGINT NOT NULL,
+    "name"              VARCHAR(64) NOT NULL REFERENCES Recipes ("name"),
+    "userid"            BIGINT NOT NULL REFERENCES Users ("id"),
+    "qty"               BIGINT NOT NULL,
     PRIMARY KEY ("name", "userid")
 );
