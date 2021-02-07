b551b6b94da77545057ee645ef5dac6fff5156c7
vcokltfre <vcokltfre@gmail.com>
2021-02-07 04:15:58 +0000

    refactor: format and touch up profile cog

--- cogs/profile.py
+++ cogs/profile.py
@@ -1,14 +1,11 @@
-import discord
+from templatebot import Bot
 from discord.ext import commands
 
 
 class Profile(commands.Cog):
-  def __init__(self, bot):
+  def __init__(self, bot: Bot):
     self.bot = bot
 
-  
-  
 
-
-def setup(bot):
+def setup(bot: Bot):
   bot.add_cog(Profile(bot))
