a6b867978a7ca1d80b060904a42c628c1e643f74
vcokltfre <vcokltfre@gmail.com>
2021-02-07 04:11:26 +0000

    feat: enable allowed mentions

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from templatebot import Bot
+from discord import AllowedMentions
 from os import environ as env
 from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-bot = Bot(name="BakerBot", command_prefix="b!")
+bot = Bot(
+    name="BakerBot",
+    command_prefix="b!",
+    allowed_mentions=AllowedMentions(
+        everyone=False, roles=False, users=False, replied_user=True
+    ),
+)
 bot.load_initial_cogs()
 
 bot.run(env.get("TOKEN", None))
